6244fe57e65e5315d24b49e4607eef17c4ade892
ErnestoPM <ernestopm20@gmail.com>
2017-04-27 07:47:16 -0500

    Adding example file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import {NativeModules} from "react-native";
 let Couchbase = NativeModules.RNCouchbaseOgm;
 
 Couchbase.init = function () {
-    console.log("khe");
+    console.log("khe pasion");
 };
 
 module.exports = Couchbase;
