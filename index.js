50ebf9632dfea15aea9fab8facd473d0b6adf23e
ErnestoPM <ernestopm20@gmail.com>
2017-04-28 19:33:29 -0500

    Adding working querying documents via promise

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ import CouchbaseClient from "react-native-couchbase-lite";
 let CouchbaseOGM = NativeModules.RNCouchbaseOgm;
 
 /**
- * Initializes the manager using the initRestClient function and saves a reference to the databaseManager.
- *
+ * Returns a new Promise that contains the manager if everything went well.
+ * @returns {Promise}
  */
-
-function init(){
+function initManager(){
     return new Promise((resolve,reject) => {
         CouchbaseClient.initRESTClient(manager => {
             if(manager == null){
@@ -21,73 +20,53 @@ function init(){
     });
 }
 
-
-async function init2(){
-    var manager = await init();
-    console.log(manager);
-}
-
-CouchbaseOGM.testFunction = function (manager) {
-    init().then(
-        function success(manager){
-            console.log(manager);
-        },
-        function error(error){
-            console.log("error");
-        }
-    );
-    //console.log(typeof CouchbaseClient.initRESTClient);
-};
-
-CouchbaseOGM.testFunction2 = function(){
-    init2();
-};
-
-
 /**
  * Creates a new database with the specified name, must conform to Couchbase Naming Rules
- *
- * @param name
+ * Awaits for the manager to be started and then returns a promise with the given result or error in resolve and reject.
+ * @param database_name
+ * @returns {Promise}
  */
-CouchbaseOGM.createDB = function(name){
-    let database_name = name;
-
-    CouchbaseClient.initRESTClient(manager => {
+CouchbaseOGM.createDB = async function(database_name){
+    let manager = await initManager();
+    return new Promise((resolve,reject) => {
         manager.database.put_db({db: database_name}).then(
-            success = (response) => {
-                console.info(response);
-                return true;
-                //console.error(response);
+            function success(response){
+                console.log("Success creating database: " + database_name);
+                resolve(response);
             },
-            error = (response) => {
-                //console.log(response);
-                console.error(response.statusText);
+            function error(response){
+                console.log("Error creating database: " + database_name);
+                reject(response);
+                return;
             }
         )
     })
 };
-
 
 /**
- * Gets all of the documents contained in that database
- *
- * @param name - The name of the database that is going to be queried to get all documents
+ * Gets all documents contained in the database with the given name
+ * Awaits for the manager to be started and then returns a promise with the given result or error in resolve and reject.
+ * @param database_name
+ * @returns {Promise}
  */
-CouchbaseOGM.getAllDocuments = function(name){
-    let database_name = name;
-    CouchbaseClient.initRESTClient(manager => {
+CouchbaseOGM.getDBDocuments = async function(database_name){
+    let manager = await initManager();
+    return new Promise((resolve,reject) => {
         manager.query.get_db_all_docs({db: database_name}).then(
-            function success(result){
-                console.log(result);
-                //console.log(JSON.parse(result.data));
-                return JSON.parse(result.data);
+            function success(response){
+                console.log("Success querying documents for database: " + database_name);
+                let result = JSON.parse(response.data);
+                resolve(result);
             },
-            function error(something){
-                console.error(something);
+            function error(response){
+                console.log("Error trying to query documents for database: " + database_name);
+                reject(response);
+                return;
             }
         )
     })
 };
+
 
 
 
